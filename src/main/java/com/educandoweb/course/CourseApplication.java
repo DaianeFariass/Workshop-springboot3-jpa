a3f77bcc74e078fbb774827a4aa25862ba88ca45
Daiane Farias <daiane.farias.contractor@gmail.com>
2024-08-31 20:36:31 +0100

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -2,12 +2,14 @@ package com.educandoweb.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourseApplication.class, args);
+		
+          SpringApplication.run(CourseApplication.class, args);
 	}
 
 }
